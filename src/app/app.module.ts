0509161aa467f5c27b27a539a9359d42858f6e1c
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-08-05 20:54:54 +0300

    Add task-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
-import { TaksComponent } from './components/taks/taks.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
-    TaksComponent,
-    TasksComponent
+    TasksComponent,
+    TaskItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
